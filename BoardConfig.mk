2feb155bbe016fb13900de6da5b15b3454f2e411
Fenix46 <scarlataemanuele@gmail.com>
2016-11-12 16:05:49 +0100

    Bring-up to build into AOSP
    Signed-off-by: Fenix46 <scarlataemanuele@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/kminilte/include
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := xyref5260_evt0_defconfig
+TARGET_KERNEL_CONFIG := kminilte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/kminilte
 
 #TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
